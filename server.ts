9d8d92dff15c9bd6dd1e3fb87e1e7cc8882ce053
MandemGibson <themaingib@gmail.com>
2024-12-12 14:29:05 +0000

    dependency installation and app initialization

---
+++ server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
